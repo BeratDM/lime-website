58749a6bb5044598d74b95badaddaf121a59877a
BeratDM <check_point2@hotmail.com>
2025-02-09 08:11:40 +0300

    added iframe to portfolio sections and visual adjustments

--- lime_website/lime_website/apps/portfolio/models.py
+++ lime_website/lime_website/apps/portfolio/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 import bleach
+from lxml import html
 
 ALLOWED_TAGS = [
     "p",
@@ -23,26 +24,55 @@ ALLOWED_TAGS = [
     "pre",
     "span",
     "div",
+    "iframe",
 ]
+
 ALLOWED_ATTRIBUTES = {
     "a": ["href", "target", "rel"],
     "img": ["src", "alt"],
     "code": ["class"],
     "span": ["class"],
     "div": ["class"],
+    "iframe": [
+        "src",
+        "width",
+        "height",
+        "allowfullscreen",
+        "frameborder",
+        "allow",
+        "referrerpolicy",
+        "sandbox",
+    ],
 }
 
+ALLOWED_PROTOCOLS = ["http", "https", "data"]
 
-def clean_html(html):
-    return bleach.clean(
-        html, tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES, strip=True
+
+DEFAULT_SANDBOX = "allow-scripts allow-same-origin allow-popups"
+
+
+def clean_html(html_content):
+    cleaned_html = bleach.clean(
+        html_content, tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES, strip=True
     )
+
+    # Parse the cleaned HTML with lxml to modify iframe attributes
+    tree = html.fragment_fromstring(cleaned_html, create_parent="div")
+
+    # Iterate over all <iframe> elements and enforce 'sandbox'
+    for iframe in tree.findall(".//iframe"):
+        if "sandbox" not in iframe.attrib:
+            iframe.attrib["sandbox"] = DEFAULT_SANDBOX  # Apply default sandbox policy
+
+    # Convert the modified tree back to a string
+    return html.tostring(tree, encoding="unicode", method="html")
 
 
 class Section(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
     description = models.TextField(blank=True, null=True)
+
     order = models.PositiveIntegerField(default=0)
 
     class Meta:
@@ -51,8 +81,8 @@ class Section(models.Model):
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
-        if self.text:
-            self.text = clean_html(self.text)
+        if self.description:
+            self.description = clean_html(self.description)
         super().save(*args, **kwargs)
 
     def __str__(self):
